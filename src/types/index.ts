12f36be374ca810eb1967d749159dabb49fdb188
edz <edz@edzdeiMac.local>
2020-01-07 20:08:48 +0800

    a

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+type Method = 'get' | 'GET' 
+| 'post' | 'POST'
+| 'delete' | 'DELETE'
+| 'head' | 'HEAD'
+| 'option' | 'OPTION'
+| 'put' | 'PUT'
+| 'patch' | 'PATCH';
+export interface AxiosRequestConfig {
+    url: string,
+    method?: Method,
+    data?: any,
+    params?: any
+}
